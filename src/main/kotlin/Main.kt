ad36bec80fa8fbf8d585a2ee3b654556699aa27e
alx66 <ru.aleks@yandex.ru>
2025-09-02 22:12:48 +0400

    Почистил проект для курса по котлину

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,4 @@
-// Напишите функцию которая принимает массив и возвращает новый массив, содеражащий только уникальные значеня и только пересекающие
-
 fun main() {
-    val x = "AEZAKMI"
-    val lower = x.replaceFirstChar { it.lowercase() }
-val test = 0
-    val test2 = 0
-    println(lower)
 
 }
 
